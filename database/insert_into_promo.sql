89493782b53513444659b07f91cc805bdd246c4c
Geoffrey <geoffrey.brunet@quartz-insight.com>
2020-11-01 17:19:56 +0100

    SQL scripts for create and insert into table are created, and they works

---
+++ database/insert_into_promo.sql
@@ -0,0 +1,8 @@
+INSERT INTO promotion (product_name, old_price, new_price, picture)
+       VALUES
+            ('Hoodie DIABLO', '60.00€', '49.49€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/product-image-699004397_2000x.jpg?v=1575933062'),
+            ('T-shirt', '40.00€', '27.90€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/t-shirt-rubber-t-shirt-futurxwear-noir-xl-2_2000x.jpg?v=1584427096'),
+            ('Sneakers RVX 1500', '70.00€', '37.00€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/product-image-916103100_2000x.jpg?v=1575933043'),
+            ('Sweat pants STRIPED RÉFLÉCHISSANT 3M', '50.00€', '37.90€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/jogging-striped-reflechissant-3mtm-pantalon-cargo-jogging-futurxwear_2000x.jpg?v=1584426332'),
+            ('Short GALACTIC', '60.OO€', '46.90€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/short-galactic-short-futurxwear-noir-s_2000x.jpg?v=1593768260'),
+            ('Jacket Denim Mountain', '90.00€', '55.00€', 'https://cdn.shopify.com/s/files/1/1968/6751/products/product-image-799643285_7c89fa7c-d634-4bbb-b54a-76ef44db2fc6_2000x.jpg?v=1575933076')
